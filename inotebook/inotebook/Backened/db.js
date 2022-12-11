5d1cfa5fc18a78dd629d7b16831e00a546df256f
Prachisharma12345 <er.prachisharma10@gmail.com>
2022-12-11 20:06:33 +0530

    Delete inotebook directory

--- inotebook/inotebook/Backened/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-const mongoURI="mongodb://localhost:27017/inotebook?readPreference=primary&directConnection=true";
-const connectToMongo=()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log("connected to mongodb");
-    })
-}
-module.exports=connectToMongo;
-
