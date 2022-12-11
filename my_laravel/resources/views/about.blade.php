138c79d1566dc77eca7ccba2097cb3d27ddd9ee4
Prachisharma12345 <er.prachisharma10@gmail.com>
2022-12-11 20:06:55 +0530

    Delete my_laravel directory

--- my_laravel/resources/views/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main');
-
-@push('title')
-    <title>About</title>
-@endpush
-@section('main-section')
-<h1 class="text-center">About Page</h1>
-@endsection
--- my_laravel/resources/views/courses.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.main');
-@push('title')
-<title>Courses</title>
-@endpush
-@section('main-section')
-    <h1 class="text-center">Courses Page</h1>
-@endsection
